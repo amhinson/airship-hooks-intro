cba7071abfdba15d2014ffbb42533b2d65ca4875
Alex Hinson <alex@teamairship.com>
2019-02-25 11:31:01 -0500

    add final examples to App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ import EffectsExampleTwo from './examples/EffectsExampleTwo';
 import EffectsMediaExample from './examples/EffectsMediaExample';
 import CustomHookExample from './examples/customHook/SearchContainer';
 import ContextExample from './examples/context/UserContainer';
+// import StateExample from "./final/StateExample";
+// import EffectsExample from "./final/EffectsExample";
+// import EffectsExampleTwo from "./final/EffectsExampleTwo";
+// import EffectsMediaExample from "./final/EffectsMediaExample";
+// import CustomHookExample from "./final/customHook/SearchContainer";
+// import ContextExample from "./final/context/UserContainer";
 
 const App = () => {
   const Example =
     //
-    // StateExample;
-    // EffectsExample;
-    // EffectsExampleTwo;
-    // EffectsMediaExample;
-    // CustomHookExample;
-    ContextExample;
+    StateExample;
+  // EffectsExample;
+  // EffectsExampleTwo;
+  // EffectsMediaExample;
+  // CustomHookExample;
+  // ContextExample;
 
   return (
     <div className="App">
